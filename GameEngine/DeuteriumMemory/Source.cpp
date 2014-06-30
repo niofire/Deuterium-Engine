f95bc94aa026a82c4089ba1ffb27da17e31fa98f
Mathieu St-Louis <stlouis_mathieu@hotmail.com>
2014-06-29 21:38:05 -0400

    d_ptr final update

--- GameEngine/DeuteriumMemory/Source.cpp
+++ GameEngine/DeuteriumMemory/Source.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include "d_ptr.h"
+#include <iostream>
+class some
+{
+public:
+	some() {}
+	~some() {}
+
+	void print() { std::cout << "HELLO FROM PRINT";}
+	void callback() { std::cout << "callback";}
+};
 
 int main()
 {
+
+	int* sd = new int(1);
+
+	d_ptr<some> g(new some);
+	d_ptr<some> d ;//= g;
+	d = g;
+	if(true)
+	{
+		d_ptr<some> asff = d;
+	}
+	d_ptr<some> q = d;
+
+	q.clear();
+	d.clear();
+	g.clear();
+	std::cout << g.num_handles() << " " << d.num_handles();// << " " << q.num_handles();
+
 	return 0;
 }
--- GameEngine/DeuteriumMemory/d_ptr.h
+++ GameEngine/DeuteriumMemory/d_ptr.h
@@ -1,51 +1,58 @@
 #pragma once
 #define NULL 0
-#define NEW(x,y) x.allocate_memory(new y);
+#include <iostream>
+/* The d_ptr class
+ *
+ *
+ *
+ *
+*/
 
 template <class T>
 class d_ptr
 	{
 	public:
 		d_ptr(void);
-		
-		void allocate_memory(T* ptr);
+
+		//constructor + allocate memory
+		d_ptr(T* ptr);
+
+		//copy constructor
+		d_ptr(d_ptr&);
+
+		//removes all references and update the number of handles
 		~d_ptr(void);
 
+		//Allocate a memory location to the d_ptr.
+		void alloc(T* ptr);
 
+		//Clears the pointer's reference and updates the number of handles
+		void clear();
 
+		int num_handles() { return *_num_handles_allocated_ptr;}
 		//------------------------------------------------
 		//			Operators
 		//------------------------------------------------
-		d_ptr<T>	&operator=(d_ptr &_ptr) {	
-				//update the num of allocated handles
-				this->_num_handles_allocated_ptr = ptr._num_handles_allocated_ptr;
-				*_num_handles_allocated_ptr += 1;
+		d_ptr	&operator=(d_ptr &ptr) {	
+			this->clear();
+			//update the num of allocated handles
+			this->_num_handles_allocated_ptr = ptr._num_handles_allocated_ptr;
+			this->add_handle();
 	
-				this->_ptr = ptr._ptr;
-				return *this;
-			};
+			this->_ptr = ptr._ptr;
+			return *this;
+		};
 
+		T*	operator->() const  {
+			return _ptr;
+		};
 
 	private:
 		void delete_unused_memory();
+		void add_handle();
 		int* _num_handles_allocated_ptr;
 		T* _ptr;
 };
-
-template <class T>
-void d_ptr<T>::allocate_memory(T* ptr)
-{
-	//checks if pointer had already a memory location allocated
-	if(_num_handles_allocated_ptr != NULL)
-	{
-		*_num_handles_allocated_ptr -= 1;
-		this->delete_unused_memory();
-	}
-
-	//store the newly allocated pointer
-	_num_handles_allocated_ptr = new int(1);
-	_ptr = ptr;
-}
 
 template <class T>
 d_ptr<T>::d_ptr()
@@ -55,6 +62,62 @@ d_ptr<T>::d_ptr()
 
 }
 
+template <class T>
+d_ptr<T>::d_ptr(T* ptr)
+{
+	_num_handles_allocated_ptr = NULL;
+	_ptr = NULL;
+
+	alloc(ptr);
+}
+template <class T>
+d_ptr<T>::d_ptr(d_ptr& ptr)
+{
+	this->clear();
+
+	//update the num of allocated handles
+	this->_num_handles_allocated_ptr = ptr._num_handles_allocated_ptr;
+	this->add_handle();
+	
+	this->_ptr = ptr._ptr;
+}
+
+template <class T>
+d_ptr<T>::~d_ptr()
+{
+	this->clear();
+}
+
+template <class T>
+void d_ptr<T>::alloc(T* ptr)
+{
+	//clear previous reference
+	this->clear();
+
+	//store the newly allocated pointer
+	_num_handles_allocated_ptr = new int(1);
+	_ptr = ptr;
+}
+
+
+template <class T>
+void d_ptr<T>::clear()
+{
+	if(this->_num_handles_allocated_ptr != NULL)
+	{
+		*_num_handles_allocated_ptr = *_num_handles_allocated_ptr - 1;
+		this->delete_unused_memory();
+	}
+
+	this->_ptr = NULL;
+	this->_num_handles_allocated_ptr = NULL;
+}
+
+template <class T>
+void d_ptr<T>::add_handle()
+{
+	*_num_handles_allocated_ptr = *_num_handles_allocated_ptr + 1;
+}
 
 template <class T>
 void d_ptr<T>::delete_unused_memory()
@@ -63,13 +126,8 @@ void d_ptr<T>::delete_unused_memory()
 	{
 		delete _ptr;
 		_ptr = NULL;
+
+		delete _num_handles_allocated_ptr;
+		_num_handles_allocated_ptr = NULL;
 	}
-}
-
-template <class T>
-d_ptr<T>::~d_ptr()
-{
-	*_num_handles_allocated_ptr -= 1;
-	this->delete_unused_memory();
-
 }
